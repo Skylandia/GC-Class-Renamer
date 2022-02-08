0dd9b656ebfbfb362c3bf08cdfd7555df9135cb4
Skylandia <52195906+Skylandia@users.noreply.github.com>
2022-02-08 16:57:29 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+// ==UserScript==
+// @name         GC Class Renamer
+// @namespace    com.skylandia
+// @version      0.1
+// @description  Allows renaming classes in Google Classroom
+// @author       Skylandia
+// @match        https://classroom.google.com/h
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
 
+let classNames = window.localStorage.getItem("class-names");
+if (classNames == null) {
+	classNames = new Map();
+}
+
+function addRenameOption() {
+	for (const node of document.getElementsByClassName("YVvGBb z3vRcc-ZoZQ1")) {
+		classNames.set(node.innerHTML, prompt("Rename: ", node.innerHTML));
+	}
+}
+
+function renameClasses() {
+    console.log("The classroom divs have loaded, renaming classes...");
+    for (const node of document.getElementsByClassName("YVvGBb z3vRcc-ZoZQ1")) {
+        node.innerHTML = classNames.get(node.innerHTML);
+    }
+}
+
+const targetNode = document.getElementById("yDmH0d"); // The body
+let isClassNameDifferent = false;
+const callback = function(mutationsList, observer) {
+    for(const mutation of mutationsList) {
+        if (isClassNameDifferent && targetNode.className != "EIlDfe kYtXye nk6WKe") {
+            isClassNameDifferent = false;
+        }
+        else if (!isClassNameDifferent && targetNode.className == "EIlDfe kYtXye nk6WKe") {
+            isClassNameDifferent = true;
+            addRenameOption();
+            renameClasses();
+            //observer.disconnect();
+        }
+        break;
+    }
+};
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, { attributes: true });
