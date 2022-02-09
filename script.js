715525db559ab815e2d788d0841e877b6a60da72
Skylandia <52195906+Skylandia@users.noreply.github.com>
2022-02-10 09:49:26 +1300

    Rename class names on the sidebar, partial cleanup

--- script.js
+++ script.js
@@ -9,33 +9,37 @@
 // @grant        none
 // ==/UserScript==
 
-var classNames = new Map();
-
-function loadClassNames() {
-    classNames = new Map(JSON.parse(window.localStorage.getItem("class-names")));
+function renameClasses() {
+    let classNames = new Map(JSON.parse(window.localStorage.getItem("class-names")));
     if (classNames.size == 0) {
         classNames = new Map();
-        renameRequest();
+        for (const node of document.getElementsByClassName("gHz6xd Aopndd rZXyy")) {
+            classNames.set(node.getAttribute("data-course-id"), [prompt("Rename title: ", node.children[0].children[2].children[1].children[0].children[0].innerHTML), prompt("Rename subititle: ", node.children[0].children[2].children[1].children[0].children[1].innerHTML)]);
+        }
+        window.localStorage.setItem("class-names", JSON.stringify(Array.from(classNames.entries())));
     }
-}
-
-function renameRequest() {
-	for (const node of document.getElementsByClassName("gHz6xd Aopndd rZXyy")) {
-		classNames.set(node.getAttribute("data-course-id"), [prompt("Rename title: ", node.children[0].children[2].children[1].children[0].children[0].innerHTML), prompt("Rename subititle: ", node.children[0].children[2].children[1].children[0].children[1].innerHTML)]);
-	}
-	window.localStorage.setItem("class-names", JSON.stringify(Array.from(classNames.entries())));
-}
-
-function renameClasses() {
     console.log("The classroom divs have loaded, renaming classes...");
     for (const node of document.getElementsByClassName("gHz6xd Aopndd rZXyy")) {
         node.children[0].children[2].children[1].children[0].children[0].innerHTML = classNames.get(node.getAttribute("data-course-id"))[0];
 	    node.children[0].children[2].children[1].children[0].children[1].innerHTML = classNames.get(node.getAttribute("data-course-id"))[1];
     }
+	for (const node of document.getElementsByClassName("Xi8cpb TMOcX")) {
+        if (!node.hasAttribute("data-id")) {
+            continue;
+        }
+		node.children[2].children[0].innerHTML = classNames.get(node.getAttribute("data-id"))[0];
+        if (node.children[2].children.length == 1) {
+            let subtitle = document.createElement("div");
+            subtitle.setAttribute("class", "TajIHf dDKhVc YVvGBb");
+            node.children[2].appendChild(subtitle);
+        }
+		node.children[2].children[1].innerHTML = classNames.get(node.getAttribute("data-id"))[1];
+	}
 }
 
 const targetNode = document.getElementById("yDmH0d"); // The body
 let isClassNameDifferent = false;
+let isSidebarDifferent = false;
 const callback = function(mutationsList, observer) {
     for(const mutation of mutationsList) {
         if (isClassNameDifferent && targetNode.className != "EIlDfe kYtXye nk6WKe") {
@@ -43,14 +47,16 @@ const callback = function(mutationsList, observer) {
         }
         else if (!isClassNameDifferent && targetNode.className == "EIlDfe kYtXye nk6WKe") {
             isClassNameDifferent = true;
-            if (classNames.size == 0) {
-                loadClassNames();
-            }
             renameClasses();
-            //observer.disconnect();
         }
-        break;
+        if (isSidebarDifferent && document.body.children[10].className != "vhK44c CBSF1e") {
+            isClassNameDifferent = false;
+        }
+        else if (!isSidebarDifferent && document.body.children[10].className == "vhK44c CBSF1e") {
+            isClassNameDifferent = true;
+            renameClasses();
+        }
     }
 };
 const observer = new MutationObserver(callback);
-observer.observe(targetNode, { attributes: true });
+observer.observe(targetNode, { attributes: true, subtree: true });
