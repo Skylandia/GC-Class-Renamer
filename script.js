48fe6df3650f0aa03cac9ad6a17de84dec3d85af
Skylandia <52195906+Skylandia@users.noreply.github.com>
2022-02-13 21:02:09 +1300

    cleaner code

--- script.js
+++ script.js
@@ -1,75 +1,97 @@
 // ==UserScript==
-// @name         GC Class Renamer
+// @name         Google Classroom Renamer
 // @namespace    com.skylandia
 // @version      0.1
-// @description  Allows renaming classes in Google Classroom
+// @description  Rename classrooms in Google Classroom
 // @author       Skylandia
-// @match        https://classroom.google.com/h
+// @match        https://classroom.google.com/*
 // @icon         https://ssl.gstatic.com/classroom/favicon.png
 // @grant        none
 // ==/UserScript==
 
-function renameClasses() {
-    let classNames = new Map(JSON.parse(window.localStorage.getItem("class-names")));
-    if (classNames.size == 0) {
-        classNames = new Map();
-        for (const node of document.getElementsByClassName("gHz6xd Aopndd rZXyy")) {
-            classNames.set(node.getAttribute("data-course-id"), [prompt("Rename title: ", node.children[0].children[2].children[1].children[0].children[0].innerHTML), prompt("Rename subititle: ", node.children[0].children[2].children[1].children[0].children[1].innerHTML)]);
-        }
-        window.localStorage.setItem("class-names", JSON.stringify(Array.from(classNames.entries())));
-    }
-    console.log("The classroom divs have loaded, renaming classes...");
-    for (const node of document.getElementsByClassName("gHz6xd Aopndd rZXyy")) {
-        node.children[0].children[2].children[1].children[0].children[0].innerHTML = classNames.get(node.getAttribute("data-course-id"))[0];
-	    node.children[0].children[2].children[1].children[0].children[1].innerHTML = classNames.get(node.getAttribute("data-course-id"))[1];
-    }
-	for (const node of document.getElementsByClassName("Xi8cpb TMOcX")) {
-        if (!node.hasAttribute("data-id")) {
-            continue;
-        }
-		node.children[2].children[0].innerHTML = classNames.get(node.getAttribute("data-id"))[0];
-        if (node.children[2].children.length == 1) {
-            let subtitle = document.createElement("div");
-            subtitle.setAttribute("class", "TajIHf dDKhVc YVvGBb");
-            node.children[2].appendChild(subtitle);
-        }
-		node.children[2].children[1].innerHTML = classNames.get(node.getAttribute("data-id"))[1];
-	}
-}
+class GCRenamer {
 
-const targetNode = document.getElementById("yDmH0d"); // The body
-let flag = [false, false, false, false];
-const callback = function(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-        if (flag[0] && targetNode.className != "EIlDfe kYtXye nk6WKe") {
-            flag[0] = false;
-        }
-        else if (!flag[0] && targetNode.className == "EIlDfe kYtXye nk6WKe") {
-            flag[0] = true;
-            renameClasses();
-        }
-        if (flag[1] && document.body.children[10].className != "vhK44c X1rxSc") {
-            flag[1] = false;
-        }
-        else if (!flag[1] && document.body.children[10].className == "vhK44c X1rxSc") {
-            flag[1] = true;
-            renameClasses();
-        }
-        if (flag[2] && document.body.children[10].className != "vhK44c CBSF1e") {
-            flag[2] = false;
-        }
-        else if (!flag[2] && document.body.children[10].className == "vhK44c CBSF1e") {
-            flag[2] = true;
-            renameClasses();
-        }
-        if (flag[3] && document.body.children[10].className != "vhK44c OMVS8d") {
-            flag[3] = false;
-        }
-        else if (!flag[3] && document.body.children[10].className == "vhK44c OMVS8d") {
-            flag[3] = true;
-            renameClasses();
-        }
+    classroomNames = new Map();
+
+    constructor() {
+        this.classroomNames.set("/c/NDY0NTMyOTE2MDI1", {title: "Mathematics", subtitle: "13MTC"});
+        this.classroomNames.set("/c/NDY0MTI4MjU0MDg1", {title: "Physics", subtitle: "13PYC"});
+        this.classroomNames.set("/c/NDY0MDY1MTg5MjU4", {title: "Computer Science", subtitle: "13CPC"});
+        this.classroomNames.set("/c/NDYyODk0MDU4ODQx", {title: "Economics", subtitle: "12ECC"});
+        this.registerObserver();
     }
-};
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, { attributes: true, subtree: true });
+    registerObserver() {
+        new MutationObserver((mutationList, observer) => {
+            for (const mutation of mutationList) {
+                // Courses
+                if ((mutation.target.getAttribute("class") == "onkcGd eDfb1d YVvGBb Vx8Sxd" || mutation.target.getAttribute("class") == "EIlDfe kYtXye nk6WKe") && mutation.type == "childList") {
+                    console.log("courses");
+                    console.log(mutation);
+
+                    let targetNodes = document.getElementsByClassName("onkcGd eDfb1d YVvGBb Vx8Sxd");
+                    for (let node of targetNodes) {
+                        node.children[0].innerHTML = this.classroomNames.get(node.getAttribute("href")).title;
+                        node.children[1].innerHTML = this.classroomNames.get(node.getAttribute("href")).subtitle;
+                    }
+                }
+                // Sidemenu
+                if (new String(mutation.target.getAttribute("class")).includes("vhK44c") && mutation.type == "attributes") {
+                    console.log("sidemenu");
+                    console.log(mutation);
+
+                    for (let child of document.getElementsByClassName("Xi8cpb TMOcX")) {
+
+                        if (child.children[2].children.length == 1) {
+                            let subtitleNode = document.createElement("div");
+                            subtitleNode.setAttribute("class", "TajIHf dDKhVc YVvGBb");
+                            child.children[2].appendChild(subtitleNode);
+                        }
+
+                        child.children[2].children[0].innerHTML = this.classroomNames.get(child.getAttribute("href")).title;
+                        child.children[2].children[1].innerHTML = this.classroomNames.get(child.getAttribute("href")).subtitle;
+                    }
+                }
+                // Class Headings & Title
+                if (new String(mutation.target.getAttribute("class")).includes("nTrDbc GAP4ve xSXax AJlUyd CPYzFb V7D6ud fKVgu nk6WKe") && mutation.type == "attributes") {
+                    // Top Heading
+                    console.log("top heading")
+                    console.log(mutation);
+
+                    let key = new String(window.location.href).replace("https://classroom.google.com", "");
+                    let headingNode = document.getElementsByClassName("onkcGd OGhwGf")[0];
+
+                    if (headingNode.children.length == 1) {
+                        let subtitleNode = document.createElement("div");
+                        subtitleNode.setAttribute("class", "YVvGBb dDKhVc Pce5Kb");
+                        headingNode.appendChild(subtitleNode);
+                    }
+
+                    headingNode.children[0].innerHTML = this.classroomNames.get(key).title;
+                    headingNode.children[1].innerHTML = this.classroomNames.get(key).subtitle;
+
+
+                    // Centre Heading
+                    console.log("centre title");
+
+                    headingNode = document.getElementsByClassName("T4tcpe")[0];
+                    console.log(headingNode);
+
+                    if (headingNode.children.length == 1) {
+                        let subtitleNode = document.createElement("div");
+                        subtitleNode.setAttribute("class", "qFmcrc z3vRcc-ZoZQ1 YVvGBb");
+                        headingNode.appendChild(subtitleNode);
+                    }
+
+                    headingNode.children[0].innerHTML = this.classroomNames.get(key).title;
+                    headingNode.children[1].innerHTML = this.classroomNames.get(key).subtitle;
+
+
+                    // Title
+                    document.title = this.classroomNames.get(key).title;
+                }
+            }
+        }).observe(document.body, {attributes: true, childList: true, subtree: true});
+    }
+}
+new GCRenamer();
+console.log("started");
